e84e68cde25392e952fe5b66341fe59213a55eeb
James Stout <james.wolf.stout@gmail.com>
2022-08-11 10:05:47 -0700

    Fix paths on windows to use backslash.

--- pull_all.ps1
+++ pull_all.ps1
@@ -1,5 +1,6 @@
+# NOTE: this script currently does not work due to https://jira.atlassian.com/browse/SRCTREEWIN-3198
 git pull origin main
-git subtree pull --prefix=.subtrees/screen-ocr --squash screen-ocr master
-git subtree pull --prefix=.subtrees/gaze-ocr --squash gaze-ocr master
-git subtree pull --prefix=.subtrees/rapidfuzz --squash rapidfuzz main
-git subtree pull --prefix=.subtrees/jarowinkler --squash jarowinkler main
+git subtree pull --prefix=.subtrees\screen-ocr --squash screen-ocr master
+git subtree pull --prefix=.subtrees\gaze-ocr --squash gaze-ocr master
+git subtree pull --prefix=.subtrees\rapidfuzz --squash rapidfuzz main
+git subtree pull --prefix=.subtrees\jarowinkler --squash jarowinkler main
