4850bc7625cb8e3648add8d46e64859949c1eed3
James Stout <james.wolf.stout@gmail.com>
2024-02-03 14:34:19 -0800

    Revert "Removed beta-only features."
    This reverts commit e4d67b6393aefcd24e054fd3fedb12a3d2f46160.

--- timestamped_captures.py
+++ timestamped_captures.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 
-from talon import Module, actions
-from talon.grammar import Phrase
+from talon import Module
 
 mod = Module()
 
@@ -28,39 +27,10 @@ class TextPosition:
     position: str
 
 
-@mod.capture(rule="<user.timestamped_phrase>+")
+@mod.capture(rule="<user.prose>")
 def timestamped_prose(m) -> TimestampedText:
     """Dictated text appearing onscreen."""
-    return TimestampedText(
-        text=" ".join([item.text for item in m]), start=m[0].start, end=m[-1].end
-    )
-
-
-# Forward to enable easy extension via a Context.
-@mod.capture(rule="<user.timestamped_phrase_default>")
-def timestamped_phrase(m) -> TimestampedText:
-    """Dictated phrase appearing onscreen."""
-    return m[0]
-
-
-# "edit" is frequently misrecognized as "at it", and is common in UIs.
-@mod.capture(
-    rule="<phrase> | {user.vocabulary} | {user.punctuation} | {user.prose_snippets} | edit"
-)
-def timestamped_phrase_default(m) -> TimestampedText:
-    """Dictated phrase appearing onscreen (default capture)."""
-    item = m[0]
-    if isinstance(item, Phrase):
-        text = " ".join(
-            actions.dictate.replace_words(actions.dictate.parse_words(item))
-        )
-        start = item.words[0].start
-        end = item.words[-1].end
-    else:
-        text = str(item)
-        start = item.start
-        end = item.end
-    return TimestampedText(text=text, start=start, end=end)
+    return TimestampedText(text=m.prose, start=m.prose_start, end=m.prose_end)
 
 
 @mod.capture(rule="[before | after] <self.timestamped_prose>")
