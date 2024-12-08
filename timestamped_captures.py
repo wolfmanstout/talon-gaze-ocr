5cf3cd357395d2224de81127006d0d3afd55b417
James Stout <james.wolf.stout@gmail.com>
2024-12-08 15:45:24 -0800

    Use "from" and "through" to indicate before or after the cursor for selections.

--- timestamped_captures.py
+++ timestamped_captures.py
@@ -92,28 +92,37 @@ def prose_range(m) -> TextRange:
     )
 
 
-@mod.capture(rule="[through] <self.prose_position>")
+@mod.capture(rule="[through | from] <self.prose_position>")
 def one_ended_prose_range(m) -> TextRange:
     """A range of onscreen text with only start or end specified."""
     has_through = m[0] == "through"
-    # As a convenience, allow dropping "through" if position is provided.
-    if has_through or m.prose_position.position:
-        if not m.prose_position.position:
-            actions.app.notify(
-                'Try "[through] before <phrase>" or "[through] after <phrase>" instead'
-                ' of "through <phrase>". The cursor position is unknown to '
-                "talon-gaze-ocr."
-            )
-            raise ValueError(
-                'Text range "through <phrase>" not supported because cursor position is unknown.'
-            )
+    has_from = m[0] == "from"
+    # As a convenience, allow dropping "through" or "from" if position is provided.
+    if m.prose_position.position:
         return TextRange(
             start=None,
             after_start=False,
             end=m.prose_position.text,
             before_end=m.prose_position.position == "before",
         )
+    elif has_through:
+        # Select current cursor point through the text after the cursor.
+        return TextRange(
+            start=None,
+            after_start=False,
+            end=m.prose_position.text,
+            before_end=False,
+        )
+    elif has_from:
+        # Select from the text before the cursor up to the current cursor point.
+        return TextRange(
+            start=None,
+            after_start=False,
+            end=m.prose_position.text,
+            before_end=True,
+        )
     else:
+        # Select the phrase itself.
         return TextRange(
             start=m.prose_position.text,
             after_start=m.prose_position.position == "after",
